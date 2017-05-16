014a20861accd3d2887892ec22cc2eff4d800921
Vitali Schneider <tastaturkaputt@googlemail.com>
2017-05-17 00:39:58 +0200

    Added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -3,7 +3,11 @@ var React = require('react');
 // Stateless functional component
 var Examples = (props) => {
   return (
-    <h3>Examples Component!</h3>
+    <div>
+      <h3>Examples Component!</h3>
+      <p>Welcome to examples page!</p>
+    </div>
+
   );
 };
 
